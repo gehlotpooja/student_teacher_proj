db1e7b7c3878f8a5f668d13feb1a95d0bf6c8d98
pooja gehlot <pooja115239@gmail.com>
2021-03-17 15:41:42 +0530

    Update settings.py

--- student_teacher_project/settings.py
+++ student_teacher_project/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'student_teacher_project.wsgi.application'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pooja115239@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hope@150715'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
